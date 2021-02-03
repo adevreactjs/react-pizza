1c81fada2e06c2d806517793edcd1fbd674f1008
adevreactjs <adevreactjs@gmail.com>
2021-02-03 22:37:20 +0200

    Add Home component

---
+++ src/components/Categories.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+
+const Categories = ({items}) => {
+
+    const [activeItem, setActiveItem] = useState(null)
+
+    const setItem = (index) => {
+        setActiveItem(index)
+    }
+
+    return (
+        <div className="categories">
+            <ul>
+                <li className={activeItem === null ? 'active': ''} onClick={() => setActiveItem(null)}>Все</li>
+                {items && items.map((name, index) => {
+                    return (
+                        <li className={activeItem === index ? 'active': ''}
+                        onClick={() => setItem(index)} 
+                        key={index}>{name}</li>
+                    )
+                })}
+            </ul>
+      </div>
+    )
+}
+
+export default Categories
